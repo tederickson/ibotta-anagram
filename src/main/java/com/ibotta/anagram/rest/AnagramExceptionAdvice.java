9c601de75fff84fc7d1aed829e1b199809901dfd
Ted Erickson <ted.erickson@comcast.net>
2024-07-15 06:23:54 -0600

    Refactor RestControllers

--- src/main/java/com/ibotta/anagram/rest/AnagramExceptionAdvice.java
+++ src/main/java/com/ibotta/anagram/rest/AnagramExceptionAdvice.java
@@ -4,16 +4,14 @@ import com.ibotta.anagram.exception.AnagramException;
 import com.ibotta.anagram.exception.InvalidWordException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class AnagramExceptionAdvice {
 
-    @ResponseBody
     @ExceptionHandler(AnagramException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     String serviceExceptionHandler(AnagramException ex) {
@@ -21,12 +19,10 @@ public class AnagramExceptionAdvice {
         return ex.getMessage();
     }
 
-    @ResponseBody
     @ExceptionHandler(InvalidWordException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     String serviceExceptionHandler(InvalidWordException ex) {
         log.warn("invalid word", ex);
         return ex.getMessage();
     }
-
 }
